e21bde63c85985b3c86f6d40e8fc90395830b8c3
afwan-sh <afwansh404@gmail.com>
2026-03-16 11:15:20 +0530

    test

--- combat_engine_v1/src/main/java/combat_engine_v1/core/GameLauncher.java
+++ combat_engine_v1/src/main/java/combat_engine_v1/core/GameLauncher.java
@@ -1,7 +1,9 @@
 package combat_engine_v1.core;
+
 import combat_engine_v1.userMenu.MainMenu;
-public class  GameLauncher{
-    public static void main(String []args){
+
+public class GameLauncher {
+    public static void main(String[] args) {
         MainMenu mainMenu = new MainMenu();
         mainMenu.Menu();
         System.out.println("Thank for playing this game");
